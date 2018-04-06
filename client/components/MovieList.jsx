0b2502695a30dfae05bc4c11a437eb248d9a7e32
seungprk@gmail.com <seungprk@gmail.com>
2018-04-05 21:31:59 -0700

    Show not found message if search finds nothing

--- client/components/MovieList.jsx
+++ client/components/MovieList.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import MovieListEntry from './MovieListEntry.jsx';
 
 var MovieList = (props) => {
+  var entries = props.movies.map(movie => <MovieListEntry movie={movie} />);
+  if (entries.length === 0) {
+    entries = <div>Movie Not Found...</div>;
+  }
   return (
     <section className="movie-list">
-      {props.movies.map(movie => <MovieListEntry movie={movie} />)}
+      {entries}
     </section>
   );
 };
