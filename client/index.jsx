1429bb99346042b0dc188d82c9c5f6ebf863e4d7
seungprk@gmail.com <seungprk@gmail.com>
2018-04-07 17:06:13 -0700

    Move dummy data to app.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App.jsx';
 
-var movies = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
-];
-
 ReactDOM.render(
-  <App movies={movies} />,
+  <App />,
   document.getElementById('app')
 );
